adce0709133641a3aeff0a492b950781d5d46a6f
Samuel Spaulding <samuel.spaulding@yale.edu>
2012-03-01 13:40:30 -0500

    Fixed version of FeedbackLoop

--- src/Primitives/Student/FeedbackLoop.cc
+++ src/Primitives/Student/FeedbackLoop.cc
@@ -7,37 +7,41 @@ void FeedbackLoop()	{
 	// Select each gesture file in dictionary to receive feedback
 	// Also, create a new file in the new directory for demo data
 	DIR *dir;
-	struct dirent *gesture;
+	struct dirent *entry;
 	dir = opendir("/data/training_data");
 	while ((entry = readdir(dir)) != NULL) {
-		
-		// Create matching_file in /data/training_data_i
-	
 		// Send that gesture to playback
-		PlaybackAndGetFeedback(entry->d_name, matching_file);
+		PlaybackAndGetFeedback(entry->d_name, "/data/training_data_1/" + entry->d_name);
 	}
 
 }
 
-void PlaybackAndGetFeedback(gesture_file, new_file) {
+void PlaybackAndGetFeedback(char *gesture_file, char *new_file) {
 
 	// Have the robot playback the action in the csv file
-	System("ext/ROS/KinectNaoPipeline/src/playback.cc", gesture_file, --nonblocking);
+        // Should this be a call to the executable?
+	System("ext/ROS/KinectNaoPipeline/src/playback.cc " + gesture_file + " --nonblocking");
 	
 	// Create feedback sensor so you can poll for feedback
-	sensor_ = new FeedbackSensor(20000);
-	sensor_->Poll();
+	sensor = new FeedbackSensor(20000);
+	sensor->Poll();
+
+        ofstream file;
+        file.open(new_file);
 	
 	// Internalize feedback 
-	if (sensor_->GetValues()[0] == 0) {		// "GOOD"
+	if (sensor->GetValues()[0] == 0) {		// "GOOD"
 		// Write a line to new_file indicating it's okay
+                file << "OK";
+                ofstream.close();
 	}
 
-	else if (sensor_->GetValues()[0] == 1 || sensor_->GetValues()[0] == 2) { // "BAD" or "PANIC"
+	else if (sensor->GetValues()[0] == 1 || sensor->GetValues()[0] == 2) { // "BAD" or "PANIC"
 		// Nao asks for demonstration
-		
-		// Capture demonstration 
-		System("ext/ROS/KinectNaoPipeline/src/capture.cc", new_file, --nonblocking);
+	
+		// Capture demonstration
+                // I think this call should be to an executable, not the .cc
+		System("ext/ROS/KinectNaoPipeline/src/capture.cc " + new_file);
 		// Run RAPTOR on new_file <- not sure where/ how to call this
 	}
 	
